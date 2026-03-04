3d18c32f35f1713149ac74a236a7f41bd92ba679
franck roussel <franck.roussel.1994@gmail.com>
2026-03-04 23:18:08 +0100

    z

--- plugins/cd4ml/cleanup/reset_database.sql
+++
@@ -1,8 +0,0 @@
-DELETE FROM experiment_tags;
-DELETE FROM latest_metrics;
-DELETE FROM metrics;
-DELETE FROM tags;
-DELETE FROM params;
-DELETE FROM runs;
-DELETE FROM experiments;
-DELETE FROM tags;
